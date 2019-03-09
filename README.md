744e66f7f95dba9ceb0c7d7b2296acd542c3d9b9
BlockBa5her <blocckba5her@gmail.com>
2019-03-09 12:30:59 -0800

    initial commit
    added resource data

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nearest Postals
+This script displays a nearest postal next to where PLD would go and also has a command to draw a route to a specific postal
+
+## Installation
+There are 2 ways to install it, and I recommend the first
+1. Run the following command in a terminal
+    - `git clone https://github.com/blockba5her/nearest-postal.git`
+2. Click the `Download` button up top to download the source code, then put it in your resources folder
+3. As of now, this script supports 2 postal maps. From what I have seen, these are the 2 most popular
+    - `new-postals.json` -> [New and Improved Postals](https://forum.fivem.net/t/release-modified-street-names-w-postal-numbers/8717)
+    - `old-postals.json` -> [Original Postals](https://forum.fivem.net/t/release-modified-street-names-w-postal-numbers/8717)
+4. To setup the postal map rename your file from what it was to `postals.json`
+
+## Command
+To draw a route to a certain postal, type `/postal [postalName]` and to remove just type `/postal`
+
+It will automatically remove the route when within 150m of the destination
+
+## Development
+This script provides a simple way of working on a new postal map
+1. In the script, enabled the local variable `dev` near the bottom
+2. Restart the script into game
+3. Teleport to the first postal code in numerical order
+4. Type `setnext [postalCode]` where postalCode is the postal that you are at
+5. Type `next` to insert it
+6. Teleport to the next postal code in numerical order
+7. Type `next` to insert it
+8. Repeat from step 6 on
+
+When done with that, you can print all of the postals you just inserted into console with the `json` command
+
+## Discord
+Join my [discord](https://discord.gg/ZcTayce) for support and more scripts
---
+++ cl.lua
@@ -0,0 +1,153 @@
+local raw = LoadResourceFile(GetCurrentResourceName(), 'postals.json')
+local postals = json.decode(raw)
+
+local nearest = nil
+local pBlip = nil
+
+local function round(val, decimal)
+    return decimal and (math.floor((val * 10^decimal) + 0.5) / (10^decimal)) or (math.floor(val + 0.5))
+end
+
+-- thread for finding nearest postal
+Citizen.CreateThread(function()
+    while true do
+        local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1)))
+
+        local nd = -1
+        local ni = -1
+        for i, p in ipairs(postals) do
+            local d = math.sqrt((x - p.x)^2 + (y - p.y)^2) -- pythagorean theorem
+            if nd == -1 or d < nd then
+                ni = i
+                nd = d
+            end
+        end
+
+        if ni ~= -1 then
+            nearest = {dist = nd, i = ni}
+        end
+
+        Wait(100)
+    end
+end)
+-- text display thread
+Citizen.CreateThread(function()
+    while true do
+        if nearest then
+            local text = ('~y~Nearest Postal~w~: %s (~g~%sm~w~)'):format(postals[nearest.i].code, tostring(round(nearest.dist, 2)))
+            SetTextScale(0.42, 0.42)
+            SetTextFont(4)
+            SetTextProportional(false)
+            SetTextColour(255, 255, 255, 255)
+            SetTextEntry("STRING")
+            SetTextCentre(0)
+            SetTextOutline()
+            AddTextComponentString(text)
+            DrawText(0.175, 0.963)
+        end
+        Wait(2)
+    end
+end)
+
+-- blip thread
+Citizen.CreateThread(function()
+    while true do
+        if pBlip then
+            local p = GetEntityCoords(GetPlayerPed(-1))
+            local b = {x = pBlip.p.x, y = pBlip.p.y}
+            local dx, dy = math.abs(p.x - b.x), math.abs(p.y - b.y)
+            local d = math.sqrt(dx^2 + dy^2)
+            if d < 100.0 then
+                RemoveBlip(pBlip.hndl)
+                pBlip = nil
+            end
+        end
+        Wait(100)
+    end
+end)
+RegisterCommand('postal', function(source, args, raw)
+    if #args < 1 then
+        if pBlip then
+            RemoveBlip(pBlip.hndl)
+            pBlip = nil
+        end
+        return
+    end
+    local n = string.upper(args[1])
+
+    local fp = nil
+    for _, p in ipairs(postals) do
+        if string.upper(p.code) == n then
+            fp = p
+        end
+    end
+
+    if fp then
+        if pBlip then
+            RemoveBlip(pBlip.hndl)
+        end
+        pBlip = {hndl = AddBlipForCoord(fp.x, fp.y, 0.0), p = fp}
+        SetBlipRoute(pBlip.hndl, true)
+        SetBlipColour(pBlip.hndl, 3)
+        SetBlipRouteColour(pBlip.hndl, 3)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentSubstringPlayerName('Postal Route '..pBlip.p.code)
+        EndTextCommandSetBlipName(pBlip.hndl)
+
+        TriggerEvent('chat:addMessage', {
+            color = {255,0,0},
+            args = {
+                'Postals',
+                'Drawing a route to '..fp.code
+            }
+        })
+    else
+        TriggerEvent('chat:addMessage', {
+            color = {255,0,0},
+            args = {
+                'Postals',
+                'That postal code doesn\'t exist'
+            }
+        })
+    end
+end)
+
+--[[Development shit]]
+local dev = false
+if dev then
+    local devLocal = json.decode(raw)
+    local next = 0
+
+    RegisterCommand('setnext', function(src, args, raw)
+        local n = tonumber(args[1])
+        if n ~= nil then
+            next = n
+            print('next '..next)
+            return
+        end
+        print('invalid '..n)
+    end)
+    RegisterCommand('next', function(src, args, raw)
+        for i, d in ipairs(devLocal) do
+            if d.code == tostring(next) then
+                print('duplicate '..next)
+                return
+            end
+        end
+        local coords = GetEntityCoords(GetPlayerPed(-1))
+        table.insert(devLocal, {code = tostring(next), x = coords.x, y = coords.y})
+        print('insert '..next)
+        next = next + 1
+    end)
+    RegisterCommand('remove', function(src, args, raw)
+        if #devLocal > 0 then
+            local data = table.remove(devLocal, #devLocal)
+            print('remove '..data.code)
+        else
+            print('invalid')
+        end
+    end)
+    RegisterCommand('json', function(src, args, raw)
+        print(json.encode(devLocal))
+    end)
+end
