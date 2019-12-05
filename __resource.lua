d42b2a02a16911dc3df7d38076f21f4f68e217bb
Jacob Patterson <16978528+blockba5her@users.noreply.github.com>
2019-12-04 21:05:06 -0800

    comment added to fix common problem

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,11 @@
 -- change it to whatever model you want that is in this directory
 local postalFile = 'new-postals.json'
 
+--[[
+WHAT EVER YOU DO, DON'T TOUCH ANYTHING BELOW UNLESS YOU **KNOW** WHAT YOU ARE DOING
+If you just want to change the postal file, **ONLY** change the above variable
+--]]
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
