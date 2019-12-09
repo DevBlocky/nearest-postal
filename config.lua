ba7db75a6e47f734e64a7f8ea08d2e0192c83ae7
BlockBa5her <blocckba5her@gmail.com>
2019-12-08 21:27:00 -0800

    change vmenu position to account for time

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
 config = {
 	text = {
-		format = "~y~Nearest Postal~w~: %s (~g~%.2fm~w~)",
+		format = '~y~Nearest Postal~w~: %s (~g~%.2fm~w~)',
 		-- ScriptHook PLD Position
 		--posX = 0.225,
 		--posY = 0.963,
 		-- vMenu PLD Position
-		posX = 0.175,
+		posX = 0.22,
 		posY = 0.963
 	},
 	blip = {
-		textFormat = "Postal Route %s",
+		textFormat = 'Postal Route %s',
 		color = 3, -- default 3 (light blue)
 		distToDelete = 100.0 -- in meters
 	}
