850c5377f24cb126005fa38664dd0e3f050d5a6d
BlockBa5her <blocckba5her@gmail.com>
2019-12-08 21:23:16 -0800

    performance improvements

--- cl.lua
+++ cl.lua
@@ -1,27 +1,40 @@
-local raw = LoadResourceFile(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), "postal_file"))
+local raw = LoadResourceFile(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), 'postal_file'))
 local postals = json.decode(raw)
 
 local nearest = nil
 local pBlip = nil
 
--- thread for finding nearest postal
+-- thread for nearest and blip
 Citizen.CreateThread(
 	function()
 		while true do
-			local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1)))
+			local x, y = table.unpack(GetEntityCoords(GetPlayerPed(-1)))
 
-			local nd = -1
-			local ni = -1
+			local ndm = -1 -- nearest distance magnitude
+			local ni = -1 -- nearest index
 			for i, p in ipairs(postals) do
-				local d = math.sqrt((x - p.x) ^ 2 + (y - p.y) ^ 2) -- pythagorean theorem
-				if nd == -1 or d < nd then
+				local dm = (x - p.x) ^ 2 + (y - p.y) ^ 2 -- distance magnitude
+				if ndm == -1 or dm < ndm then
 					ni = i
-					nd = d
+					ndm = dm
 				end
 			end
 
+			--setting the nearest
 			if ni ~= -1 then
-				nearest = {dist = nd, i = ni}
+				local nd = math.sqrt(ndm) -- nearest distance
+				nearest = {i = ni, d = nd}
+			end
+
+			-- if blip exists
+			if pBlip then
+				local b = {x = pBlip.p.x, y = pBlip.p.y} -- blip coords
+				local dm = (b.x - x) ^ 2 + (b.y - y) ^ 2 -- distance magnitude
+				if dm < config.blip.distToDelete ^ 2 then
+					-- delete blip if close
+					RemoveBlip(pBlip.hndl)
+					pBlip = nil
+				end
 			end
 
 			Wait(100)
@@ -33,41 +46,24 @@ Citizen.CreateThread(
 	function()
 		while true do
 			if nearest then
-				local text = config.text.format:format(postals[nearest.i].code, nearest.dist)
+				--print(json.encode(nearest))
+				local text = config.text.format:format(postals[nearest.i].code, nearest.d)
 				SetTextScale(0.42, 0.42)
 				SetTextFont(4)
 				SetTextProportional(false)
-				SetTextEntry("STRING")
+				SetTextEntry('STRING')
 				SetTextCentre(0)
 				SetTextOutline()
 				AddTextComponentString(text)
 				DrawText(config.text.posX, config.text.posY)
 			end
-			Wait(2)
+			Wait(0)
 		end
 	end
 )
 
--- blip thread
-Citizen.CreateThread(
-	function()
-		while true do
-			if pBlip then
-				local p = GetEntityCoords(GetPlayerPed(-1))
-				local b = {x = pBlip.p.x, y = pBlip.p.y}
-				local dx, dy = math.abs(p.x - b.x), math.abs(p.y - b.y)
-				local d = math.sqrt(dx ^ 2 + dy ^ 2)
-				if d < config.blip.distToDelete then
-					RemoveBlip(pBlip.hndl)
-					pBlip = nil
-				end
-			end
-			Wait(100)
-		end
-	end
-)
 RegisterCommand(
-	"postal",
+	'postal',
 	function(source, args, raw)
 		if #args < 1 then
 			if pBlip then
@@ -93,27 +89,27 @@ RegisterCommand(
 			SetBlipRoute(pBlip.hndl, true)
 			SetBlipColour(pBlip.hndl, 3)
 			SetBlipRouteColour(pBlip.hndl, config.blip.color)
-			BeginTextCommandSetBlipName("STRING")
+			BeginTextCommandSetBlipName('STRING')
 			AddTextComponentSubstringPlayerName(config.blip.textFormat:format(pBlip.p.code))
 			EndTextCommandSetBlipName(pBlip.hndl)
 
 			TriggerEvent(
-				"chat:addMessage",
+				'chat:addMessage',
 				{
 					color = {255, 0, 0},
 					args = {
-						"Postals",
-						"Drawing a route to " .. fp.code
+						'Postals',
+						'Drawing a route to ' .. fp.code
 					}
 				}
 			)
 		else
 			TriggerEvent(
-				"chat:addMessage",
+				'chat:addMessage',
 				{
 					color = {255, 0, 0},
 					args = {
-						"Postals",
+						'Postals',
 						"That postal code doesn't exist"
 					}
 				}
@@ -129,64 +125,64 @@ if dev then
 	local next = 0
 
 	RegisterCommand(
-		"setnext",
+		'setnext',
 		function(src, args, raw)
 			local n = tonumber(args[1])
 			if n ~= nil then
 				next = n
-				print("next " .. next)
+				print('next ' .. next)
 				return
 			end
-			print("invalid " .. n)
+			print('invalid ' .. n)
 		end
 	)
 	RegisterCommand(
-		"next",
+		'next',
 		function(src, args, raw)
 			for i, d in ipairs(devLocal) do
 				if d.code == tostring(next) then
-					print("duplicate " .. next)
+					print('duplicate ' .. next)
 					return
 				end
 			end
 			local coords = GetEntityCoords(GetPlayerPed(-1))
 			table.insert(devLocal, {code = tostring(next), x = coords.x, y = coords.y})
-			print("insert " .. next)
+			print('insert ' .. next)
 			next = next + 1
 		end
 	)
 	RegisterCommand(
-		"rl",
+		'rl',
 		function(src, args, raw)
 			if #devLocal > 0 then
 				local data = table.remove(devLocal, #devLocal)
-				print("remove " .. data.code)
-				print("next " .. next)
+				print('remove ' .. data.code)
+				print('next ' .. next)
 				next = next - 1
 			else
-				print("invalid")
+				print('invalid')
 			end
 		end
 	)
 	RegisterCommand(
-		"remove",
+		'remove',
 		function(src, args, raw)
 			if #args < 1 then
-				print("invalid")
+				print('invalid')
 			else
 				for i, d in ipairs(devLocal) do
 					if d.code == args[1] then
 						table.remove(devLocal, i)
-						print("remove " .. d.code)
+						print('remove ' .. d.code)
 						return
 					end
 				end
-				print("invalid")
+				print('invalid')
 			end
 		end
 	)
 	RegisterCommand(
-		"json",
+		'json',
 		function(src, args, raw)
 			print(json.encode(devLocal))
 		end
