e912b93d2da9f5d9d7e562d68ecaa2fbadfe7e32
Michael Spector <43636601+MichaelCoding25@users.noreply.github.com>
2021-01-28 12:07:33 +0200

    Added url to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,6 +12,7 @@ games { 'gta5' }
 author 'blockba5her'
 description 'This script displays a nearest postal next to where PLD would go and also has a command to draw a route to a specific postal'
 version '1.4.1'
+url 'https://github.com/blockba5her/nearest-postal'
 
 client_scripts {
 	'config.lua',
